20b201dbd8234a25035f5fd9320aa144a6f1ac79
Reahana <reahana233@gmail.com>
2024-01-07 20:28:03 +0600

    app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { useEffect, useState } from 'react';
 import Countries from './components/Countries/Countries';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
@@ -7,39 +6,11 @@ import Footer from './components/Footer/Footer';
 function App() {
   return (
     <div className="App">
-      {/* <LoadCountries></LoadCountries> */}
       <Header></Header>
       <Countries></Countries>
       <Footer></Footer>
     </div>
   );
 } 
-
-// function LoadCountries(){
-//   const [countries, setCountries] = useState([])
-//   useEffect(()=>{
-//     fetch('https://restcountries.com/v3.1/all')
-//     .then(res=>res.json())
-//     .then(data => setCountries(data))
-//   } , [])
-//   return(
-//     <div>
-//       <h1>Visiting every country of the world </h1>
-//       <h3>All countries {countries.length}</h3>
-//       {
-//         countries.map(country=> <Country name={country.name.common} population={country.population}></Country>)
-        
-//       }
-//     </div>
-//   )
-// }
-// function Country(props){
-//   return(
-//     <div>
-//       <h2>Name: {props.name}</h2>
-//       <h4>Population: {props.population}</h4>
-//     </div>
-//   )
-// }
 
 export default App;
